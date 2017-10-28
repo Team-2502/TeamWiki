74ca599a4430fd635c9555facdab57adfc02223e
Andrew Gazelka <andrew.gazelka@gmail.com>
2017-10-28 14:05:20 -0500

    add messages to init script

--- workspace_setup_init.sh
+++ workspace_setup_init.sh
@@ -1,6 +1,10 @@
+echo '> Starting init script'
 rm -rf /tmp/TeamWikiInstallation
 mkdir -p /tmp/TeamWikiInstallation
+echo '> Curling main script'
 curl -s https://raw.githubusercontent.com/Team-2502/TeamWiki/master/workspace_setup_main.sh -o /tmp/TeamWikiInstallation/workspace_setup.sh
 chmod +x /tmp/TeamWikiInstallation/workspace_setup.sh
+echo '> Executing main script'
 /tmp/TeamWikiInstallation/workspace_setup.sh
+echo '> Removing all script garbage'
 rm -rf /tmp/TeamWikiInstallation
